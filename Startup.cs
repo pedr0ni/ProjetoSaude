ed582078d8d2f355f76693d20c93c6a4d734cb9b
Matheus Pedroni <pedroni.dev@gmail.com>
2018-09-03 19:11:27 -0300

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -30,21 +30,21 @@ namespace ProjetoSaude
         {
 
 
-            /*Use para Banco de Dados InMemory
+            /*Use para Banco de Dados InMemory */
             services.AddEntityFrameworkInMemoryDatabase().AddDbContext<IDatabaseContext>(options =>
             {
                 options.UseInMemoryDatabase("SaudeDb");
-            });*/
+            });
 
 
             /*
              * Use para Banco de Dados MySql.
              * Connection String definida no appsettings.json
-             */
+             *
             services.AddDbContext<IDatabaseContext>(options =>
             {
                 options.UseMySql(Configuration["ConnectionString"]);
-            });
+            });*/
 
 
             /*
